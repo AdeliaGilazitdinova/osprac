aea40d9e03c1579839244abec7c1788c963ebe03
AdeliaGilazutdinova <77272545+AdeliaGilazutdinova@users.noreply.github.com>
2021-02-22 16:19:46 +0300

    Update main.c

--- Sem5-6_2/task3/task3/main.c
+++ Sem5-6_2/task3/task3/main.c
@@ -2,24 +2,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <fcntl.h>
 int main() {
     int fd[2];
     int size=0;
-    
+
     if(pipe(fd)<0)
     {
         printf("ошибка");
         exit(1);
     }
-    else
-    {
-        
-        while (write(fd[1], "a", 2) == 2) {
-            size += 2;
-            printf("%d\n", size);
-        }
+    fcntl(fd[1], F_SETFL, fcntl(fd[1], F_GETFL) | O_NONBLOCK);
+    while (write(fd[1], "a", 2) == 2) {
+        size += 2;
     }
-    
+    printf("%d\n",size);
+
     return 0;
 }
